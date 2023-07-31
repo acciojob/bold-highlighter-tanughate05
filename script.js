b0bd0fa424eb13d7e7109f099f2b6704cc6af055
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-31 15:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+window.onload = getBold_items();
+ 
+// 
+function getBold_items() 
+{
+  bold_Items = document.getElementsByTagName('strong'); 
+}
+
 function highlight() {
-    //Write your code here
-
-
+    //Write your code herevar bold_Items;
+   for (var i=0; i<bold_Items.length; i++)
+   {                                                    
+    bold_Items[i].style.color = "green";
+    }
 }
 
 
 function return_normal() {
     //Write your code here
-
+ for (var i=0; i<bold_Items.length; i++) 
+  {
+       bold_Items[i].style.color = "black";
+  }
     
 }
